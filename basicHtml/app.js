349dc2ae844316e79ba96ae673c27ee6b4c2e989
ICE <mohamedsamir22446688@gmail.com>
2023-01-20 11:13:42 +0200

    added js code

---
+++ basicHtml/app.js
@@ -0,0 +1,74 @@
+const btn1 = document.getElementById("btn-0");
+const btn2 = document.getElementById("btn-1");
+const btn3 = document.getElementById("btn-2");
+const btn4 = document.getElementById("btn-3");
+const btn5 = document.getElementById("btn-4");
+const btn6 = document.getElementById("btn-5");
+const btn7 = document.getElementById("btn-6");
+const btn8 = document.getElementById("btn-7");
+const btn9 = document.getElementById("btn-8");
+const btn10 = document.getElementById("btn-9");
+
+const btn11 = document.getElementById("btn-+");
+
+let i;
+let n;
+
+btn1.addEventListener("click", ()=> {
+    console.log("0 button clicked")  
+    let i = 0;
+    console.log("i = " + i)
+});
+btn2.addEventListener("click", ()=> {
+    console.log("1 button clicked")
+    let i = 1;
+    console.log("i = " + i)  
+});
+btn3.addEventListener("click", ()=> {
+    console.log("2 button clicked")
+    let i = 2;
+    console.log("i = " + i)
+});
+btn4.addEventListener("click", ()=> {
+    console.log("3 button clicked")
+    let i = 3;
+    console.log("i = " + i)
+});
+btn5.addEventListener("click", ()=> {
+    console.log("4 button clicked")
+    let i = 4;
+    console.log("i = " + i)
+});
+btn6.addEventListener("click", ()=> {
+    console.log("5 button clicked")
+    let i = 5
+    console.log("i = " + i)
+});
+btn7.addEventListener("click", ()=> {
+    console.log("6 button clicked")
+    let i = 6
+    console.log("i = " + i)
+});
+btn8.addEventListener("click", ()=> {
+    console.log("7 button clicked")
+    let i = 7;
+    console.log("i = " + i)
+});
+btn9.addEventListener("click", ()=> {
+    console.log("8 button clicked")
+    let i = 8;
+    console.log("i = " + i)
+});
+btn10.addEventListener("click", ()=> {
+    console.log("9 button clicked")
+    let i = 9;
+    console.log("i = " + i)
+});
+
+if(btn11.addEventListener("click", ()=> {
+    console.log("add button clicked")
+})){
+    console.log("if condition working")
+} 
+
+   
